37a46213a5b28590488c1a80ecba180a347b55ae
Ilyesse <elqardaoui@live.fr>
2025-09-02 23:45:47 +0200

    Fixer

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/3dactiv' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/3dactiv' : '',
   images: {
     unoptimized: true,
     domains: ['localhost'],
