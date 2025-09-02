a1b49aca437b874cc423321c15af0ebded48d792
Ilyesse <elqardaoui@live.fr>
2025-09-02 23:55:27 +0200

    Fixer

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/3dactiv' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/3dactiv' : '',
   images: {
     unoptimized: true,
     domains: ['localhost'],
+  },
+  // Forcer la copie des assets statiques
+  experimental: {
+    outputFileTracingRoot: undefined,
   },
 };
 
