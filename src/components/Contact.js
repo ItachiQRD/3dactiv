03ccd16bdc6610617aeb839f4ca8b24038d17d35
Ilyesse <elqardaoui@live.fr>
2025-09-04 00:13:41 +0200

    Final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -310,7 +310,7 @@ const Contact = () => {
           >
             <div className="h-96 w-full">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.2376854854047!2d2.2285230115629693!3d48.87274529957723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e664d8dbc439cf%3A0x8282e902a42522c7!2s14%20Rue%20Edouard%20Nieuport%2C%2092150%20Suresnes!5e0!3m2!1sfr!2sfr!4v1756936584540!5m2!1sfr!2sfr" 
+                src="https://maps.google.com/maps?q=14+Rue+Edouard+Nieuport,+92150+Suresnes,+France&t=&z=15&ie=UTF8&iwloc=&output=embed" 
                 width="100%" 
                 height="100%" 
                 style={{ border: 0 }} 
